791c6b87b7cef368f47721af3e8418da5a2236f1
Alexander Carvalho <alexandersilva193@gmail.com>
2025-02-11 21:12:23 -0300

    PR correção

--- src/DevIO.Api/V2/Controllers/TesteController.cs
+++ src/DevIO.Api/V2/Controllers/TesteController.cs
@@ -17,40 +17,5 @@ namespace DevIO.Api.V2.Controllers
         {
             _logger = logger;
         }
-
-        [HttpGet]
-        public string Valor()
-        {
-
-            throw new Exception("Error");
-
-            try
-            {
-                var i = 0;
-                var result = 42 / i;
-            }
-            catch (DivideByZeroException e)
-            {
-                e.Ship(HttpContext);
-            }
-
-            _logger.LogTrace("Log de Trace");
-            _logger.LogDebug("Log de Debug");
-            _logger.LogInformation("Log de Informação");
-            _logger.LogWarning("Log de Aviso");
-            _logger.LogError("Log de Erro");
-            _logger.LogCritical("Log de Problema Critico");
-
-            return "Sou a V2";
-        }
-
-        [HttpGet]
-        public string RotaAleatoria()
-        {
-
-            var secret = "o&W$SuESmsgY%dJiHYs^m6QdFXM8^D89%XNu336oxs^yx9cA5@eRKd^UrnRMM8wf@RRNrCB";
-
-            return secret;
-        }
     }
 }
